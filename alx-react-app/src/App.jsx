6caf2a069db06e27b407680b1ca813a42084a44c
Godwin Nyavor <nyavorgodwin@gmail.com>
2026-01-10 12:31:36 +0000

    REACT props

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from "./Header";
 import MainContent from "./MainContent";
 import Footer from "./Footer";
+import UserProfile from "./components/UserProfile";
 import WelcomeMessage from "./WelcomeMessage";
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
@@ -11,6 +12,7 @@ function App() {
   return (
     <div>
       <Header />
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
       <MainContent />
       <Footer />
     </div>
