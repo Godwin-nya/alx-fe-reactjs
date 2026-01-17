ceb911fd3da75a0f4d918555f5d8526db63d4101
Godwin Nyavor <nyavorgodwin@gmail.com>
2026-01-17 16:50:00 +0000

    Context API

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,28 @@
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import WelcomeMessage from "./components/WelcomeMessage";
+import UserContext from "./UserContext";
+import "./App.css";
+
+function App() {
+  const userData = {
+    name: "Alice",
+    age: 25,
+    bio: "Loves hiking and photography",
+  };
+  return (
+    <UserContext.Provider value={userData}>
+      <div>
+        <Header />
+        <WelcomeMessage />
+        <UserProfile />
+        <MainContent />
+        <Footer />
+      </div>
+    </UserContext.Provider>
+  );
+}
+
+export default App;
