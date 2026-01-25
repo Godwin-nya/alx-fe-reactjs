fd38bd374072c24e9dca7e05cc5c9d8818a11a9f
Godwin Nyavor <nyavorgodwin@gmail.com>
2026-01-25 12:05:03 +0000

    Favorites state

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,6 +3,38 @@ import { create } from "zustand";
 const useRecipeStore = create((set) => ({
   recipes: [],
 
+  favorites: [], // array of recipe IDs
+
+  // Add recipe ID to favorites
+  addFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: state.favorites.includes(recipeId)
+        ? state.favorites
+        : [...state.favorites, recipeId],
+    })),
+
+  // Remove recipe ID from favorites
+  removeFavorite: (recipeId) =>
+    set((state) => ({
+      favorites: state.favorites.filter((id) => id !== recipeId),
+    })),
+
+  // ===== RECOMMENDATIONS =====
+  recommendations: [],
+
+  // Mock recommendation logic:
+  // pick some non-favorite recipes randomly
+  generateRecommendations: () => {
+    const { recipes, favorites } = get();
+
+    const recommended = recipes.filter(
+      (recipe) =>
+        !favorites.includes(recipe.id) && Math.random() > 0.5
+    );
+
+    set({ recommendations: recommended });
+  },}))
+
   // ADD
   addRecipe: (newRecipe) =>
     set((state) => ({
