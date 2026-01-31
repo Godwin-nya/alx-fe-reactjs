bc27468ad9a19d83ca0478069df162d675dd721f
Godwin Nyavor <nyavorgodwin@gmail.com>
2026-01-31 15:43:21 +0000

    Github User search repo added

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,9 +1,23 @@
+import{Routes, Route} from "react-router-dom"
+import Home from "./pages/Home";
+import Profile from "./pages/Profile";
+
 function App() {
     
   return (
-    <div>
-      <h1>GitHub User Finder</h1>
-      <p>Project setup complete </p>
+    <div className="app-container">
+      <header>
+        <h1>GitHub User Finder</h1>
+        <p>Project setup complete </p>
+      </header>
+
+      {/* Main_contents */}
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/profile/:username" element={<Profile />} />
+        </Routes>
+      </main>
     </div>
   );
 }
---
+++ github-user-search/src/pages/Profile.jsx
@@ -0,0 +1,7 @@
+function Profile() {
+    return ( 
+        <h2>This is a user Profile.</h2>
+     );
+}
+
+export default Profile;
