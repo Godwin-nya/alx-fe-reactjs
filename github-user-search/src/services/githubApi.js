377be1b07b61dc9e29f9b2dd5e05499897391df9
Godwin Nyavor <nyavorgodwin@gmail.com>
2026-01-31 18:12:01 +0000

    Github User Search

--- github-user-search/src/services/githubApi.js
+++ github-user-search/src/services/githubApi.js
@@ -2,9 +2,9 @@ import axios from "axios";
 
 
 const githubApi = axios.create({
-    baseURL: "https://api.github.com",
-    headers: {Authorization:`token ${import.meta.env.VITE_APP_GITHUB_API_KEY}`,
-}
+    baseURL: "https://api.github.com"
+   
+
 })
 
 export default githubApi;
